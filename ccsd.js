751bb6ca4fd87ae73b5d4efe004fb87dc3b7a187
Darksourav <souravroy13095@gmail.com>
2023-04-22 23:16:03 +0530

    adding to git

---
+++ ccsd.js
@@ -0,0 +1,13 @@
+const signupForm = document.querySelector('.signup form');
+signupForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+	// validate form inputs here
+	// if valid, submit form to server
+});
+
+const loginForm = document.querySelector('.login form');
+loginForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+	// validate form inputs here
+	// if valid, submit form to server
+});
